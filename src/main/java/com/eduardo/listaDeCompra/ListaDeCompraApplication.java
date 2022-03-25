cf54a2655df42b860efb3f04b5b95da001e52235
Eduardo1749 <dudu.evangelista1749@gmail.com>
2022-03-25 16:45:36 -0300

    projeto iniciado

---
+++ src/main/java/com/eduardo/listaDeCompra/ListaDeCompraApplication.java
@@ -0,0 +1,14 @@
+package com.eduardo.listaDeCompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeCompraApplication {
+
+	// Essa é a classe que irá rodar sempre iniciarmos o nosso projeto
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeCompraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/listaDeCompra/ListaDeCompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.listaDeCompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeCompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
